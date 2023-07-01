74b842d27f9990e478f5e24928f3958483b51744
Gervais Amoah <gervaisamoah@yahoo.com>
2023-07-01 13:24:15 +0000

    Build the Sidebar menu

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { styled } from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -7,8 +9,16 @@ const StyledSidebar = styled.aside`
   border-right: 1px solid var(--color-grey-100);
 
   grid-row: 1/ -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
 `;
 
 export default function Sidebar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
