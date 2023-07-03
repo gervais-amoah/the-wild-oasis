c60e5569ef0cce48751ccb023351862097c525b5
Gervais Amoah <gervaisamoah@yahoo.com>
2023-07-03 18:52:01 +0000

    add fetch cabins service

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://tcpygydqvycpfkfjxicc.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjcHlneWRxdnljcGZrZmp4aWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyMTg5MTYsImV4cCI6MjAwMzc5NDkxNn0.L0vCR4l5rRWCs3EZnWTGYs_EcZZYhsUDRalbGY_g-mc";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
