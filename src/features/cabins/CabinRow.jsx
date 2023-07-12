aaf2600b481004533a475f055c86a4934cb77edf
Gervais Amoah <gervaisamoah@yahoo.com>
2023-07-12 11:22:37 +0000

    Fetch Cabin data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -38,3 +38,20 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+import React from "react";
+import { formatCurrency } from "../../utils/helpers";
+
+export default function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  return (
+    <TableRow>
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,23 +1,17 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins()
-      .then((data) => console.log("Data received \n", data))
-      .catch((err) => console.log("Some error occures\n", err));
-  });
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <img
-        src="https://tcpygydqvycpfkfjxicc.supabase.co/storage/v1/object/public/cabin-images/cabin-008.jpg"
-        alt="Cabins interior"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
