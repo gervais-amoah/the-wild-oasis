5dce233deb9bdd72b6b2d7d7b0734313b6735bd7
Gervais Amoah <gervaisamoah@yahoo.com>
2023-07-18 14:02:33 +0000

    Fix  Authentication issue after logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       toast.success("Successfully logged in");
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
