ee2e0dde31dd07f9d8b61d53d19bab2b95a13f9e
Gervais Amoah <gervaisamoah@yahoo.com>
2023-07-18 20:53:36 +0000

    Display stats on the dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,30 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+import React from "react";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
+
+export default function DashboardLayout() {
+  const { isLoading, bookings, numDays } = useRecentBookings();
+  const { isLoading: loadingStays, stays } = useRecentStays();
+
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  if (isLoading || loadingStays || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={stays}
+        cabinsCount={cabins.length}
+        numDays={numDays}
+      />
+    </StyledDashboardLayout>
+  );
+}
