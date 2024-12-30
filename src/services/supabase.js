3fc6d55eb08c98a727e4c90e62b57d952bde20f2
gervais <gervaisamoah@yahoo.com>
2024-12-30 15:50:10 +0000

    Add default values for supabase variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  'https://txxsnmejijdynesxaijs.supabase.co';
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHNubWVqaWpkeW5lc3hhaWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ1OTk1OTQsImV4cCI6MjA1MDE3NTU5NH0.WohEIJ89kWMQOIFzbWbZcT6cjn-eHwFBGfI8TUTvL6E';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
